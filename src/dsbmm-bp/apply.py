8bcc0296b5e8f745915f540f1a0157eb6405c49c
John Fitzgerald <j.a.fitzgerald@outlook.com>
2022-02-15 15:50:40 +0000

    initial repo structure

---
+++ src/dsbmm-bp/apply.py
@@ -0,0 +1,25 @@
+import .simulation 
+import .data_processer
+import .base 
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # Simulate data
+    
+    
+    
+    # Process
+    
+    
+    
+    # Initialise model 
+    
+    
+    
+    # Apply model 
