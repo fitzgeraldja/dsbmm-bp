c4e50b23a9ab4909bc56c05aa072fafa2fa842fe
John Fitzgerald <j.a.fitzgerald@outlook.com>
2022-10-06 20:30:10 +0100

    minor alts

--- src/dsbmm_bp/np_par_bp_methods.py
+++ src/dsbmm_bp/np_par_bp_methods.py
@@ -1294,9 +1294,24 @@ class NumpyBP:
         )
 
         # calc last term
-        marg_sums = self.node_marg.sum(axis=0)
-        last_term = np.einsum("qrt,tq,tr->", self.model._beta, marg_sums, marg_sums)
-        last_term /= self.model._tot_N_pres
+        if not self.deg_corr:
+            marg_sums = self.node_marg.sum(axis=0)
+            last_term = np.einsum("qrt,tq,tr->", self.model._beta, marg_sums, marg_sums)
+            last_term /= self.model._tot_N_pres
+        else:
+            out_deg_weighted_marg_sums = (
+                self.node_marg * self.model.degs[:, :, 1][..., np.newaxis]
+            ).sum(axis=0)
+            in_deg_weighted_marg_sums = (
+                self.node_marg * self.model.degs[:, :, 0][..., np.newaxis]
+            ).sum(axis=0)
+            last_term = np.einsum(
+                "qrt,tq,tr->",
+                self.model._lam,
+                out_deg_weighted_marg_sums,
+                in_deg_weighted_marg_sums,
+            )
+            last_term /= self.model._tot_N_pres
 
         # if self.verbose:
         # print("Spatial link energy: ", f_spatlink)
